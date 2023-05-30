bd39da43319160a04b30b84c1c24e4a315b7fa69
strawberry-fury <96857954+strawberry-fury@users.noreply.github.com>
2023-05-30 01:24:13 -0400

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,61 @@
+Game.registerMod('HoldClicker', {
+    condition: {
+        mouseDown: false,
+        mouseHover: false,
+    },
+
+    config: {
+        speed: 30,
+    },
+
+    interval: undefined,
+
+    init: function() {
+        console.log(this)
+        // Game.customOptionsMenu.push(this.addOptionsMenu);
+        this.conditionDetect();
+        }
+
+    },
+
+    load: function(str) {
+        const config = JSON.parse(str);
+        for(const c in config) this.config[c] = config[c];
+    },
+
+    save: function() {
+        return JSON.stringify(this.config);
+    },
+
+    conditionDetect: function() {
+        const mod = this
+        document.body.onmousedown = function(e) {
+            if (e.button == 0) {
+                mod.condition.mouseDown = true
+                mod.clicker(mod)
+            }
+        }
+        document.body.onmouseup = function(e) {
+            if (e.button == 0) {
+                mod.condition.mouseDown = false
+                mod.clicker(mod)
+            }
+        }
+        document.getElementById('bigCookie').addEventListener('mouseover', function () {
+            mod.condition.mouseHover = true
+            mod.clicker(mod)
+        });
+        document.getElementById('bigCookie').addEventListener('mouseout', function () {
+            mod.condition.mouseHover = false
+            mod.clicker(mod)
+        });
+    },
+
+    clicker: function (mod) {
+        if (mod.condition.mouseDown && mod.condition.mouseHover) {
+            mod.interval = setInterval(function () {Game.ClickCookie(0)}, 1000/mod.config.speed);
+        } else {
+            clearInterval(mod.interval);
+        }
+    }
+});
